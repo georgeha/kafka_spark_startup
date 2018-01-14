db43ef4a9214a15070ac9e0bae0a778f64ec2940
George Chantzialexiou <george.chantzialexiou@gmail.com>
2018-01-14 12:57:43 -0600

    scripts

--- batch_script.sh
+++ batch_script.sh
@@ -4,6 +4,8 @@ module load jdk64/1.8.0
 
 SCHEDULER=`hostname`
 hostnodes=`scontrol show hostnames $SLURM_NODELIST`
+brokers_no=1
+spark_no=16
 echo $hostnodes
 
 #SETUP SPARK
@@ -14,17 +16,21 @@ echo 'export PYSPARK_PATH=/home/03662/tg829618/anaconda2/bin/python' >> spark-en
 echo 'export SPARK_MASTER_HOST='$SCHEDULER >> spark-env.sh
 echo 'Spark Submit endpoint: ' $SCHEDULER:7077
 
+cd /home/03662/tg829618/experiments-sbatch/
+echo 'Starting Spark Cluster..'
+python start_spark.py $spark_no
 
 #SETUP Kafka and start Zookeeper
 
 cd /home/03662/tg829618/experiments-sbatch/
 echo 'starting Zookeeper..'
 cd /home/03662/tg829618/experiments-sbatch/kafka_2.11-0.8.2.1 		
-./bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
-echo 'zk is running'
+#./bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
+echo 'Zookeeper is running'
 echo 'zookeeper.connect:' $SCHEDULER:2181
 
 #Start kafka brokers
-brokers_no=1
 cd /home/03662/tg829618/experiments-sbatch/
 python start_brokers.py $brokers_no 
+
+#./spark-2.2.0-bin-hadoop2.7/bin/spark-submit --executor-memory 100g  spark-2.2.0-bin-hadoop2.7/examples/src/main/python/pi.py
